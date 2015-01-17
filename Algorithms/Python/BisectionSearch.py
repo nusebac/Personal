600078a58532482a317ab170e284707a5762babc
Arka Gupta <a0120039@nus.edu.sg>
2015-01-18 01:26:19 +0800

    Update BisectionSearch.py
    Bisecting the search for Guess and check into two parts and then again iterating based on region that satisfies the condition.It saves a lot of computation time than Guess & Check using approximation method.

--- Algorithms/Python/BisectionSearch.py
+++ Algorithms/Python/BisectionSearch.py
@@ -2,4 +2,18 @@
 """
 @author: Arka
 """
-print "None"
+
+enter = int(raw_input("Please enter a number : "))
+
+error=0.01
+low=0.0
+high = enter
+result=(low + high)/2.0
+
+while abs(result**2 - enter) >= error:
+    if result**2 < enter:
+        low=result
+    else:
+        high =result
+    result =(low + high)/2.0
+print "Hey its near to the square root of %s"%(str(result))
