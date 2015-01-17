172458df798bdca24ee5edea962043fa58ca23a9
Arka Gupta <nusebac@users.noreply.github.com>
2015-01-18 01:55:43 +0800

    Update Newton_Raphson.py
    This is the most elegant method for finding roots in the cheapest computational cost.

--- Algorithms/Python/Newton_Raphson.py
+++ Algorithms/Python/Newton_Raphson.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 18 01:47:59 2015
 
+@author: HP
+"""
+# Newton-Raphson for finding roots
+
+enter =int(raw_input("Enter a value : "))
+
+error=0.01
+result = enter/2.0
+while (abs(result*result) - enter) > error:
+    result =result - ((result**2)-enter) /(2*result)
+
+print "The square root is near to : %s"%(str(result))
